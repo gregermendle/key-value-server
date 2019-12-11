97cb0996719da1767e3b4294d2b4704a2e48eddd
Greg <gjordan@umassd.edu>
2019-12-11 01:08:06 -0500

    setup server skeleton

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import * as express from 'express';
+import { AddressInfo } from 'net';
+
+const app = express();
+
+app.get('/set', () => {
+  // Get key and value from query parameters here
+});
+
+const server = app.listen(3000, () => {
+  const addressInfo = server.address() as AddressInfo; 
+  console.log(`Server connected at ${addressInfo.address}:${addressInfo.port}`)
+});
