9feb0f49523176637cc4e57297f22a6fd63f81c2
Joe Jacobi <joseph.p.jacobi@gmail.com>
2019-12-12 14:47:15 -0500

    endpoint set up, tested and connected to front end

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -1,15 +1,42 @@
 import * as React from 'react';
+import { string } from 'prop-types';
 
 const Form = () => {
   const [keyInput, setKeyInput] = React.useState('');
   const [valueInput, setValueInput] = React.useState('');
+  const [message, setMessage] = React.useState('');
 
+  const newInput = (key: string, value: string) => {
+    //If key or value is empty, set message to both fields are required
+    const input = {
+      key, 
+      value
+    };
+
+    fetch('http://localhost:3000/store',
+      {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(input)
+      })
+      .then(response => response.json())
+      .then(result => setMessage(result.message))
+      .catch(err => console.log(err))
+  }
+  
   const submitForm = React.useCallback(() => {
-    // Submit form logic here
-  }, []);
+      // Submit form logic here
+      newInput(keyInput, valueInput)
+    }, 
+    [keyInput, valueInput],
+  );
+
 
   return (
     <div>
+      {message}
       <label>Key:</label>
       <input
         placeholder="Key" 
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,22 @@
 import * as express from 'express';
 import { AddressInfo } from 'net';
+import * as bodyParser from 'body-parser';
 
 const app = express();
 
-app.get('/store', () => {
+app.use(bodyParser.json());
+
+//Change name to be more specific
+app.post(`/store`, function(req, res) {
   // Get key and value from query parameters here
+  console.log(req.body);
+  
+  const { key, value } = req.body;
+  //connect to database 
+  //save to database
+  //if successful message = sucessfully created
+  //if duplicate message = key already exists
+  res.send({ message: 'successfully created' })
 });
 
 const server = app.listen(3000, () => {
